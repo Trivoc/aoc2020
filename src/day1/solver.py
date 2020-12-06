774b01590102c1a218d8913e931a3ef3f702e448
unknown <trivoc92@gmail.com>
2020-12-06 15:29:21 +0100

    tidied up Day 1

--- src/day1/solver.py
+++ src/day1/solver.py
@@ -1,32 +1,23 @@
+import itertools
+
+
 def read_input():
     with open("./input.txt", "r") as input_file:
         return [line.strip() for line in input_file]
 
 
 def solve_part_1(input):
-    for index_outer in range(1, len(input)):
-        for index_inner in range(index_outer, len(input)):
-            inner = int(input[index_inner])
-            outer = int(input[index_outer])
-            if inner + outer == 2020:
-                return inner * outer
+    return [int(a) * int(b) for a,b in itertools.combinations(input, 2) if int(a) + int(b) == 2020].pop()
 
 
 def solve_part_2(input):
-    for index_outer in range(1, len(input)):
-        for index_inner in range(index_outer, len(input)):
-            for index_innest in range(index_inner, len(input)):
-                innest = int(input[index_innest])
-                inner = int(input[index_inner])
-                outer = int(input[index_outer])
-                if inner + outer + innest == 2020:
-                    return inner * outer * innest
-
+    return [int(a) * int(b) * int(c) for a,b,c in itertools.combinations(input, 3)
+            if int(a) + int(b) + int(c) == 2020].pop()
 
 
 input = read_input()
 print(input)
-solution1 = solve_part_1(input)
-solution2 = solve_part_2(input)
 print(f'Part 1 solution: {solve_part_1(input)}')
-print(f'Part 2 solution: {solution2}')
+print(f'Part 2 solution: {solve_part_2(input)}')
+
+
