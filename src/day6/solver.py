a90eba9c2ecb15f7694bc1c6f0a8ae969ad9caca
unknown <trivoc92@gmail.com>
2020-12-06 10:31:40 +0100

    Day 6 fixes

--- src/day6/solver.py
+++ src/day6/solver.py
@@ -1,13 +1,9 @@
 from functools import reduce
 
-def read_input():
+
+def read_input(split_on):
     with open("input.txt", "r") as input_file:
-        return input_file.read()
-
-
-def group_input(input_string):
-    return input_string.split('\n\n')
-
+        return input_file.read().strip().split(split_on)
 
 
 def solve_part_1(groups):
@@ -16,19 +12,18 @@ def solve_part_1(groups):
     return sum(unique_no)
 
 
-
 def solve_part_2(groups):
     grouped_answers = list(map(lambda string: set(string.split('\n')), groups))
     print(grouped_answers)
     sum = 0
     for answer_set in grouped_answers:
         list_of_charsets = list(set(a) for a in answer_set)
-        intersect =(reduce(lambda x, y: x.intersection(y), list_of_charsets))
+        intersect = reduce(lambda x, y: x.intersection(y), list_of_charsets)
+        sum += len(intersect)
     return sum
 
 
-input = read_input()
-group_answers = group_input(input)
-print(group_answers)
-print(f'Part 1 solution: {solve_part_1(group_answers)}')
-print(f'Part 2 solution: {solve_part_2(group_answers)}')
+input = read_input('\n\n')
+print(input)
+print(f'Part 1 solution: {solve_part_1(input)}')
+print(f'Part 2 solution: {solve_part_2(input)}')
